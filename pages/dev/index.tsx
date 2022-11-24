9b5c63abf519b8c71a4b65a3ddd1ddbc44218f79
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-11-24 11:37:12 +0545

    chore: folder structure change

--- pages/dev/index.tsx
+++ pages/dev/index.tsx
@@ -1,14 +1,5 @@
+const DevelopmentHome = () => {
+  return <div>DevelopmentHome</div>;
+};
 
-function Dev() {
-  return (
-    <div>
-        <button className="btn btn-primary">Red</button>
-
-        <div className="">
-            <p className="text-primary">Hello</p>
-        </div>
-    </div>
-  )
-}
-
-export default Dev
+export default DevelopmentHome;
