183dd193232eafd9dcba807f92c09a8fe7d0f205
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-11-24 12:17:05 +0545

    chore: folder structure change

--- features/docs/components/buttons/primary-button.tsx
+++ features/docs/components/buttons/primary-button.tsx
@@ -1,5 +1,7 @@
+import styles from './primary-button.module.scss';
+
 const PrimaryButton = () => {
-  return <div>PrimaryButton</div>;
+  return <h2 className={`${styles.customElement}`}>Primary-Button</h2>;
 };
 
 export default PrimaryButton;
