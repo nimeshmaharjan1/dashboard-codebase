7a1838a6f7c4eafe80eff828e8bfb8c1d4ac0f49
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-11-25 11:06:40 +0545

    feat(cms): folder structure

--- features/cms/components/layout/main/header/index.tsx
+++ features/cms/components/layout/main/header/index.tsx
@@ -1,5 +1,15 @@
+//Import module scss as styles
+
+import styles from './header.module.scss';
+
 const Header = () => {
-  return <div>Header</div>;
+  //You can concatenate or just give one styles such as shown below
+  return (
+    <>
+      <h1 className={`${styles.custom_header_title} mb-2`}>Header Title</h1>
+      <p className={styles.custom_paragraph}>Paragraph Text</p>
+    </>
+  );
 };
 
 export default Header;
