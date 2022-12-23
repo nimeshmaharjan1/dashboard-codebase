5db71470f6243d7db4850861425cdfc9dad8d0cc
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-12-23 13:02:19 +0545

    feat(fake-api): implementing fake api with json placeholder

---
+++ features/home/data/tree-data.ts
@@ -0,0 +1,25 @@
+export const initialTreeData = [
+  {
+    key: '0001',
+    title: 'Donuts',
+    children: [
+      { key: '5001', title: 'None' },
+      { key: '5002', title: 'Glazed' },
+      { key: '5005', title: 'Sugar' },
+      { key: '5007', title: 'Powdered Sugar' },
+      { key: '5006', title: 'Chocolate with Sprinkles' },
+      { key: '5003', title: 'Chocolate' },
+      { key: '5004', title: 'Maple' },
+    ],
+  },
+  {
+    key: '0002',
+    title: 'Cake',
+    children: [
+      { key: '1001', title: 'Regular' },
+      { key: '1002', title: 'Chocolate' },
+      { key: '1003', title: 'Blueberry' },
+      { key: '1004', title: "Devil's Food" },
+    ],
+  },
+] as any;
--- pages/users/user-tree/index.tsx
+++ pages/users/user-tree/index.tsx
@@ -1,33 +1,8 @@
+import { initialTreeData } from '@features/home/data/tree-data';
 import { NextPageWithLayout } from '@pages/_app';
 import MainLayout from '@shared/layouts/main';
 import { Tree } from 'antd';
-import { ReactElement, useState } from 'react';
-
-const initialTreeData = [
-  {
-    key: '0001',
-    title: 'Donuts',
-    children: [
-      { key: '5001', title: 'None' },
-      { key: '5002', title: 'Glazed' },
-      { key: '5005', title: 'Sugar' },
-      { key: '5007', title: 'Powdered Sugar' },
-      { key: '5006', title: 'Chocolate with Sprinkles' },
-      { key: '5003', title: 'Chocolate' },
-      { key: '5004', title: 'Maple' },
-    ],
-  },
-  {
-    key: '0002',
-    title: 'Cake',
-    children: [
-      { key: '1001', title: 'Regular' },
-      { key: '1002', title: 'Chocolate' },
-      { key: '1003', title: 'Blueberry' },
-      { key: '1004', title: "Devil's Food" },
-    ],
-  },
-] as any;
+import React, { ReactElement, useState } from 'react';
 
 const updateTreeData = (list: any, key: React.Key, children: any): any =>
   list.map((node: any) => {
