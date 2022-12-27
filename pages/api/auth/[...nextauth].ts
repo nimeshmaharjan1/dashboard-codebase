183fa431c9bf6b3c7f6919e434ba3e849eed94a1
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-12-27 13:15:22 +0545

    ci: switch to yarn for package management

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,19 +4,13 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
 
       credentials: {
         // username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
@@ -24,19 +18,6 @@ export const authOptions = {
       },
 
       async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        // const res = await fetch('/your/endpoint', {
-        //   method: 'POST',
-        //   body: JSON.stringify(credentials),
-        //   headers: { 'Content-Type': 'application/json' },
-        // });
-        // const user = await res.json();
-
         const { username, password } = credentials as ILogin;
 
         // If no error and we have user data, return it
@@ -57,7 +38,6 @@ export const authOptions = {
         };
       },
     }),
-    // ...add more providers here
   ],
   pages: {
     signIn: '/auth/login',
